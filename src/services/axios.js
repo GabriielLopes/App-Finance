37d22554b1a7a72ae48522d97ab88cd217a0e349
Gabriel Lopes <Gabrielcostaplay@gmail.com>
2024-08-09 13:30:00 -0300

    Alteração da baseUrl

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// https://api-finance-zeta.vercel.app/
+
 export default axios.create({
-  baseURL: 'http://localhost:9001',
+  baseURL: 'https://api-finance-zeta.vercel.app/',
 });
